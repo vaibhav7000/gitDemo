c7f403249d2bb85713e8c1fb763da3a5a6368bbf
vaibhav7000 <vchawla7000@gmail.com>
2024-05-21 13:42:14 +0530

    read and write file feature is added

---
+++ main.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+function writeFile(data){
+    return new Promise(function(resolve,reject){
+        fs.writeFile('a.txt',data,function(err){
+            if(err){
+                reject('error is present');
+            } else{
+                resolve(true);
+            }
+        })
+    })
+}
+
+function readFile(){
+    fs.readFile('a.txt','utf-8',function(err,data){
+        if(err){
+            console.log('error is present');
+        } else{
+            console.log(data);
+        }
+    })
+}
+
+writeFile('Hi there').then(readFile).catch(function(err){
+    console.log(err);
+})
